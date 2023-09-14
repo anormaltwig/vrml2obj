a87479b321e51277630577db4f3146496910169e
ANormalTwig <1andonlytwig@gmail.com>
2023-09-14 14:27:31 -0400

    "finish" it

--- vrml2obj.lua
+++ vrml2obj.lua
@@ -1,19 +1,20 @@
 local shapeCounter = 0
 
-function ParseFile(filename, offset)
+local function parseFile(filename, offX, offY, offZ)
+	offX, offY, offZ = offX or 0, offY or 0, offZ or 0
+
 	local file = assert(io.open(filename))
 	local content = file:read("*a")
 
 	local shapes = {}
 
 	for transform in string.gmatch(content, "Transform%s*(%b{})") do
-		local sx, sy, sz = string.match(transform, "translation%s+(-?%d+%.?%d*)[%s,](-?%d+%.?%d*)[%s,](-?%d+%.?%d*)")
+		local tsx, tsy, tsz = string.match(transform, "translation%s+(-?%d+%.?%d*)[%s,](-?%d+%.?%d*)[%s,](-?%d+%.?%d*)")
 		local rsx, rsy, rsz, ang = string.match(transform, "rotation%s+(-?%d+%.?%d*)[%s,](-?%d+%.?%d*)[%s,](-?%d+%.?%d*)[%s,](-?%d+%.?%d*)")
-		local offX, offY, offZ = offset[1] or 0, offset[2] or 0, offset[3] or 0
-		if sx then
-			offX = tonumber(sx) + offX
-			offY = tonumber(sy) + offY
-			offZ = tonumber(sz) + offZ
+		if tsx then
+			offX = tonumber(tsx) + offX
+			offY = tonumber(tsy) + offY
+			offZ = tonumber(tsz) + offZ
 		end
 
 		for inline in string.gmatch(transform, "Inline%s*(%b{})") do
@@ -21,7 +22,7 @@ function ParseFile(filename, offset)
 			subFile = string.sub(subFile, 2, #subFile - 1)
 
 			local cwd = string.match(filename, ".+/")
-			ParseFile(cwd .. subFile, {offX, offY, offZ})
+			parseFile(cwd .. subFile, offX, offY, offZ)
 		end
 
 		for s in string.gmatch(transform, "IndexedFaceSet%s+(%b{})") do
@@ -34,14 +35,14 @@ function ParseFile(filename, offset)
 
 				if rsx then
 					local ca, sa = math.cos(ang), math.sin(ang)
-					local x1, y1, z1, x2, y2, z2 = tonumber(rsx), tonumber(rsy), tonumber(rsz), x, y, z
+					local x1, y1, z1, x2, y2, z2 = tonumber(rsx), tonumber(rsy), tonumber(rsz), x - offX, y - offY, z - offZ
 
 					local length = (x1 * x1 + y1 * y1 + z1 * z1)^0.5
 					x1, y1, z1 = x1 / length, y1 / length, z1 / length
 
-					x = (ca + (x1^2) * (1-ca)) * x2 + (x1 * y1 * (1-ca) - z1 * sa) * y2 + (x1 * z1 * (1-ca) + y1 * sa) * z2
-					y = (y1 * x1 * (1-ca) + z1 * sa) * x2 + (ca + (y1^2) * (1-ca)) * y2 + (y1 * z1 * (1-ca) - x1 * sa) * z2
-					z = (z1 * x1 * (1-ca) - y1 * sa) * x2 + (z1 * y1 * (1-ca) + x1 * sa) * y2 + (ca + (z1^2) * (1-ca)) * z2
+					x = (ca + (x1^2) * (1-ca)) * x2 + (x1 * y1 * (1-ca) - z1 * sa) * y2 + (x1 * z1 * (1-ca) + y1 * sa) * z2 + offX
+					y = (y1 * x1 * (1-ca) + z1 * sa) * x2 + (ca + (y1^2) * (1-ca)) * y2 + (y1 * z1 * (1-ca) - x1 * sa) * z2 + offY
+					z = (z1 * x1 * (1-ca) - y1 * sa) * x2 + (z1 * y1 * (1-ca) + x1 * sa) * y2 + (ca + (z1^2) * (1-ca)) * z2 + offZ
 				end
 
 				table.insert(vertices, {x + offX, y + offY, z + offZ})
@@ -49,7 +50,11 @@ function ParseFile(filename, offset)
 
 			local indexes = {}
 			for n in string.gmatch(coordIndex, "(-?%d+%.?%d*)") do
-				table.insert(indexes, n)
+				if n == "-1" then
+					table.insert(indexes, -1)
+				else
+					table.insert(indexes, tonumber(n) + 1)
+				end
 			end
 
 			table.insert(shapes, {vertices = vertices, indexes = indexes})
@@ -65,20 +70,36 @@ function ParseFile(filename, offset)
 
 		local buf = {}
 		for _, coord in ipairs(shape.indexes) do
-			if coord ~= "-1" then
-				table.insert(buf, tonumber(coord) + 1)
+			if coord ~= -1 then
+				table.insert(buf, coord)
 			else
 				obj = string.format("%sf %s\n", obj, table.concat(buf, " "))
 				buf = {}
 			end
 		end
 
-		local out = assert(io.open(string.format("objs/%s%x.obj", string.match(filename, "/([^/]+)%..+$"), shapeCounter), "w"))
+		local out = assert(io.open(string.format("%s/%s%x.obj", OUTPUT_DIRECTORY, string.match(filename, "/([^/]+)%..+$"), shapeCounter), "w"))
 		out:write(obj)
 
 		shapeCounter = shapeCounter + 1
 	end
 end
 
-ParseFile("./MIL-coast.wrl", {0, 0, 0})
+-- CLI
+local argparse = require("argparse")
+
+local parser = argparse("vrml2obj", "oh you know...")
+parser:argument("input", "Input VRML file.")
+parser:option("-o --output", "Target output directory", "vrml_objs")
+
+local args = parser:parse()
+
+OUTPUT_DIRECTORY = args.output
+-- Create output directory
+os.execute(string.format("mkdir %s", OUTPUT_DIRECTORY))
+
+print(string.format("Reading file '%s'", args.input))
+print(string.format("Writing output to '%s/'", OUTPUT_DIRECTORY))
+
+parseFile(args.input)
 
